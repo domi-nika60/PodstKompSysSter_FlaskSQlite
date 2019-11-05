0779c3af86ef8b3b480008e88553025fcedc5097
domi-nika60 <dominikafurgala1@gmail.com>
2019-11-06 00:35:34 +0100

    Create all tables

--- pkssLogs.py
+++ pkssLogs.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
-# from models import db
 from json import dumps
 from flask_marshmallow import Marshmallow
 
@@ -18,7 +17,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pkssAdmin:pkssAdmin1@logs.cegwdkw512mn.us-east-2.rds.amazonaws.com/logs'
 app.debug = True
 db = SQLAlchemy(app)
-# db.init_app(app) # dla managerów
+ma = Marshmallow(app)
 
 # -------------------------- Classes with models ----------------------------------
 class BaseModel(db.Model):
@@ -45,115 +44,159 @@ class BaseModel(db.Model):
         }
 
 class Timer(BaseModel, db.Model):
-    """Model for the timers table"""
     __tablename__ = 'timer'
-
     id = db.Column(db.Integer, primary_key = True)
-    time = db.Column(db.Float)
-    log = db.Column(db.Float)
+    timpestamp = db.Column(db.String)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    email = db.Column(db.String(120))
-
-    def __init__(self, username, email):
-        self.username= username
-        self.email = email
+    def __init__(self, timestamp):
+        self.timestamp= timestamp
 
     def __repr__(self):
-        return '<User %r>' &self.username
+        return '<User %r>' &self.timestamp
 
-# class UserSchema(ma.Schema):
-#     class Meta:
-#         # Fields to expose
-#         fields = ('username', 'email')
-
-# user_schema = UserSchema()
-# users_schema = UserSchema(many=True)
-
-class LogsAll(db.Model):
-    _tablename_='Logs_all'
+class Dostawca(db.Model):
+    _tablename_='Dostawca'
     id = db.Column(db.Integer, primary_key=True)
-    info_type = db.Column(db.String(80))
-    content = db.Column(db.String(200))
-    time_stamp = db.Column(db.String(80))
-    # service_id = db.relationship("Regulator", backref="logger")
+    stan = db.Column(db.String(120))
+    strumien_ogrzanej_wody_Fzm = db.Column(db.String(80))
+    temp_wody_zasil_Tzm = db.Column(db.String(80))
+    temp_zewn_To = db.Column(db.String(80))
+    awaria = db.Column(db.Boolean)
+    timestamp = db.Column(db.String(80))
 
-    def __init__(self, info_type, content, time_stamp):
-        # self.service_id = service_id
-        self.info_type = info_type
-        self.content = content
-        self.time_stamp = time_stamp
+    def __init__(self, stan, strumien_ogrzanej_wody_Fzm, temp_wody_zasil_Tzm, temp_zewn_To, awaria, timestamp):
+        self.stan = stan
+        self.strumien_ogrzanej_wody_Fzm = strumien_ogrzanej_wody_Fzm
+        self.temp_wody_zasil_Tzm = temp_wody_zasil_Tzm
+        self.temp_zewn_To = temp_zewn_To
+        self.awaria = awaria
+        self.timestamp = timestamp
 
 class Regulator(db.Model):
     _tablename_='Regulator'
     id = db.Column(db.Integer, primary_key=True)
-    temp_zewn = db.Column(db.String(80))
-    zmiana_stanu = db.Column(db.String(120))
-    time_stamp = db.Column(db.String(80))
-    # logger_id = db.Column(db.Integer, db.ForeignKey('logsall.id'))
+    stan = db.Column(db.String(120))
+    temp_wody_wpływającej_Tzco = db.Column(db.String(80))
+    temp_zadana_Tzcoref = db.Column(db.String(80))
+    zawór = db.Column(db.String(20))
+    timestamp = db.Column(db.String(80))
 
-    def __init__(self, temp_zewn, zmiana_stanu, time_stamp):
-        self.temp_zewn = temp_zewn
-        self.zmiana_stanu = zmiana_stanu
-        self.time_stamp = time_stamp
-        # self.logger_id = logger_id
+    def __init__(self, stan, temp_wody_wpływającej_Tzco, temp_zadana_Tzcoref, zawór, timestamp):
+        self.stan = stan
+        self.temp_wody_wpływającej_Tzco = temp_wody_wpływającej_Tzco
+        self.temp_zadana_Tzcoref = temp_zadana_Tzcoref
+        self.zawór = zawór
+        self.timestamp = timestamp
+
+class RegSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("stan", "temp_wody_wpływającej_Tzco", "temp_zadana_Tzcoref", "zawór", "timestamp")
+
+reg_schema = RegSchema()
+regs_schema = RegSchema(many=True)
+
+class Wymiennik(db.Model):
+    _tablename_='Wymiennik'
+    id = db.Column(db.Integer, primary_key=True)
+    stan = db.Column(db.String(120))
+    timestamp = db.Column(db.String(80))
+    
+    def __init__(self, stan, timestamp):
+        self.stan = stan
+        self.timestamp = timestamp
+
+class Budynek1(db.Model):
+    _tablename_='Budynek1'
+    id = db.Column(db.Integer, primary_key=True)
+    stan = db.Column(db.String(120))
+    pobór_wody_Fcob = db.Column(db.String(80))
+    temp_powrotna_Tpcob = db.Column(db.String(80))
+    temp_kaloryferow_Th = db.Column(db.String(80))
+    temp_pomieszczen_Tr = db.Column(db.String(80))
+    timestamp = db.Column(db.String(80))
+
+    def __init__(self, stan, pobór_wody_Fcob, temp_powrotna_Tpcob, temp_kaloryferow_Th, temp_pomieszczen_Tr, timestamp):
+        self.stan = stan
+        self.pobór_wody_Fcob = pobór_wody_Fcob
+        self.temp_powrotna_Tpcob = temp_powrotna_Tpcob
+        self.temp_kaloryferow_Th = temp_kaloryferow_Th
+        self.temp_pomieszczen_Tr = temp_pomieszczen_Tr
+        self.timestamp = timestamp
 
 #-----------------------------------------------------------------------
 #                             ROUTING
 #-----------------------------------------------------------------------
 @app.route('/')
 def index():
-    return "<h1 style='color': red'>hello flask with postgresql</h1>"
-
-ma = Marshmallow(app)
+    return "<h1 style='color': red'>Database!</h1>"
 
 #----------------------------- USER --------------------------
-@app.route('/post_user', methods=['POST'])
-def post_user():
-    username = request.json['username']
-    email = request.json['email']
-    new_user = User(username, email)
-    db.session.add(new_user)
-    db.session.commit()
-    return ("The new user was added!")
+# @app.route('/post_user', methods=['POST'])
+# def post_user():
+#     username = request.json['username']
+#     email = request.json['email']
+#     new_user = User(username, email)
+#     db.session.add(new_user)
+#     db.session.commit()
+#     return ("The new user was added!")
 
-@app.route("/user", methods=["GET"])
-def get_user():
-    all_users = User.query.all()
-    result = users_schema.dump(all_users)
-    return jsonify(result)
+# @app.route("/user", methods=["GET"])
+# def get_user():
+#     all_users = User.query.all()
+#     result = users_schema.dump(all_users)
+#     return jsonify(result)
 
-@app.route("/user/<id>", methods=["GET"])
-def user_detail(id):
-    user = User.query.get(id)
-    return user_schema.dump(user)
+# @app.route("/user/<id>", methods=["GET"])
+# def user_detail(id):
+#     user = User.query.get(id)
+#     return user_schema.dump(user)
 
 # ----------------------- LOGGING_ALL ----------------------------------
-@app.route('/log_all', methods=['POST'])
-def log_all():
-    # service_id = request.json['service_id']
-    info_type = request.json['info_type']
-    content = request.json['content']
-    time_stamp = request.json['time_stamp']
+# @app.route('/log_all', methods=['POST'])
+# def log_all():
+#     # service_id = request.json['service_id']
+#     info_type = request.json['info_type']
+#     content = request.json['content']
+#     time_stamp = request.json['time_stamp']
 
-    new_log_all = LogsAll(info_type, content, time_stamp)
-    db.session.add(new_log_all)
-    db.session.commit()
-    return ("New log was received and worte to database")
+#     new_log_all = LogsAll(info_type, content, time_stamp)
+#     db.session.add(new_log_all)
+#     db.session.commit()
+#     return ("New log was received and worte to database")
 
 # ----------------------- REGULATOR ----------------------------------
 @app.route('/regulator/log', methods=['POST'])
 def log_regulator():
-    temp_zewn = request.json['temp_zewn']
-    zmiana_stanu = request.json['zmiana_stanu']
-    time_stamp = request.json['time_stamp']
+    stan = request.json['stan']
+    temp_wody_wpływającej_Tzco = request.json['temp_wody_wpływającej_Tzco']
+    temp_zadana_Tzcoref = request.json['temp_zadana_Tzcoref']
+    zawór = request.json['zawór']
+    timestamp = request.json['timestamp']
 
-    new_log_reg = LOGS_ALL(temp_zewn, zmiana_stanu, time_stamp)
+    new_log_reg = Regulator(stan, temp_wody_wpływającej_Tzco, temp_zadana_Tzcoref, zawór, timestamp)
     db.session.add(new_log_reg)
     db.session.commit()
     return ("New log of regulator was received and worte to database")
+
+@app.route("/regulator", methods=["GET"])   #get all data
+def get_reg():
+    all_regs = Regulator.query.all()
+    result = regs_schema.dump(all_regs)
+    return jsonify(result)
+
+@app.route("/regulator/id/<id>", methods=["GET"])   #get id
+def get_regId(id):
+    reg = Regulator.query.get(id)
+    return reg_schema.jsonify(reg)
+
+@app.route("/regulator/last/<num>", methods=["GET"])   #get last x records
+def get_regLast(num):
+    records = Regulator.query.filter().order_by(Regulator.id.desc()).limit(num).all()
+    result = regs_schema.dump(records)
+    return jsonify(result)
+
+
 
 if __name__ == '__main__':
     app.run()
